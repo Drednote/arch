6f60e98328ae13d22ec737be7a38457e63937769
Ivan Galushko <galushko.ivan8@gmail.com>
2024-03-26 22:27:24 +0300

    mvp release

--- source/startWeston.sh
+++ source/startWeston.sh
@@ -1,8 +1,9 @@
  #!/bin/env sh
  
- export XDG_CURRENT_DESKTOP=weston
- export XDG_SESSION_DESKTOP=weston
- export XDG_SESSION_TYPE=wayland
- export WLR_NO_HARDWARE_CURSORS=1
- export WLR_RENDERER_ALLOW_SOFTWARE=1
- exec weston --shell=fullscreen-shell.so
+export XDG_CURRENT_DESKTOP=weston
+export XDG_SESSION_DESKTOP=weston
+export XDG_SESSION_TYPE=wayland
+export WLR_NO_HARDWARE_CURSORS=1
+export WLR_RENDERER_ALLOW_SOFTWARE=1
+
+exec weston --shell=fullscreen-shell.so
